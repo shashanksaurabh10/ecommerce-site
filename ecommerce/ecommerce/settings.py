8ea76f5ad2c190bf387bb489e6c710d5f398df39
shashank <shashankb.18.beis@acharya.ac.in>
2022-04-28 19:22:48 +0530

    django authentication

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -133,3 +133,14 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger'
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'shashanksingh29022020@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangoproject2022'
+EMAIL_USE_TLS = True
